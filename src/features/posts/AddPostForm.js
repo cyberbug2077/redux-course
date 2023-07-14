8938e6f54efe296cf4c0913adc457bb9105e937e
ash <nengxuj@gmail.com>
2023-07-14 18:58:55 +0800

    move state fomatting to prepare callback

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
 
 import { postAdded } from "./postsSlice";
 
@@ -12,13 +11,7 @@ const AddPostForm = () => {
     const onSavePostClicked = () => {
         if (title && content) {
             dispatch(
-                postAdded(
-                    {
-                        id: nanoid(),
-                        title,
-                        content
-                    }
-                )
+                postAdded(title, content)
             )
             setContent('')
             setTitle('')
