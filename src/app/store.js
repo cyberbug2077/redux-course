42e3476bff43a90238c2b79a0d50751fef309ba9
ash <nengxuj@gmail.com>
2023-07-14 19:44:30 +0800

    add usersSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/postsSlice'
+import usersRuducer from '../features/users/usersSlice'
 
 export const store = configureStore({
     reducer: {
-        posts: postsReducer       
+        posts: postsReducer,       
+        users: usersRuducer, 
     }
 })
--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,22 +1,33 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
+    const dispatch = useDispatch()
+
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
-    const dispatch = useDispatch()
+    const [ userId, setUserId ] = useState('')
+
+    const users = useSelector(selectAllUsers)
 
     const onSavePostClicked = () => {
         if (title && content) {
             dispatch(
-                postAdded(title, content)
+                postAdded(title, content, userId)
             )
             setContent('')
             setTitle('')
         }
     }
+
+    const usersOptions = users.map( user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
 
     return (
         <section>
@@ -29,6 +40,10 @@ const AddPostForm = () => {
                     value={title}
                     onChange={e => setTitle(e.target.value)}
                 />
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" value={userId} onChange={e => setUserId( e.target.value )}>
+                    { usersOptions }
+                </select>
                 <label htmlFor="postContent">Content:</label>
                 <textarea
                     type="text"
